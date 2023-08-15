7ab319b60846d95b029bf23060dd664a5fe2f552
Surdiana <dwisurdiana88@gmail.com>
2023-08-15 21:57:23 +0700

    uws dan int only

--- utils.go
+++ utils.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -38,4 +40,60 @@ func CreateJWT(secret, id, name, email, telp string, verify bool, ex time.Durati
 	}
 	tokenStr, err := token.SignedString(secret)
 	return tokenStr, err
+}
+
+func IntOnly(str string) (int, error) {
+	// Membagi string menjadi angka-angka dan operator
+	var nums []string
+	var operators []string
+	num := ""
+	for _, char := range str {
+		if char >= '0' && char <= '9' {
+			num += string(char)
+		} else if IsOperator(char) {
+			if num != "" {
+				nums = append(nums, num)
+				num = ""
+			}
+			operators = append(operators, string(char))
+		}
+	}
+	if num != "" {
+		nums = append(nums, num)
+	}
+
+	// Menghitung hasil berdasarkan operator
+	result, err := strconv.Atoi(nums[0])
+	if err != nil {
+		return 0, err
+	}
+	for i := 0; i < len(operators); i++ {
+		num, err := strconv.Atoi(nums[i+1])
+		if err != nil {
+			return 0, err
+		}
+		operator := operators[i]
+		switch operator {
+		case "+":
+			result += num
+		case "-":
+			result -= num
+		case "*":
+			result *= num
+		case "/":
+			result /= num
+		}
+	}
+
+	// Menampilkan hasil
+	return result, nil
+}
+
+func Uws(input string) string {
+	return strings.ReplaceAll(input, "_", " ")
+}
+
+func IsOperator(char rune) bool {
+	operators := "+-*/!@#$%^&()-=" // Anda dapat menambahkan operator lain yang mungkin digunakan
+	return strings.ContainsRune(operators, char)
 }
